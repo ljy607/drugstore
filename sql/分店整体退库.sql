8df472dc89266c1af2f6882553a6e7b417f6f5b7
ljy607 <26521794@qq.com>
2017-08-16 17:36:59 +0800

    2017年8月16日 17:36:56

---
+++ sql/分店整体退库.sql
@@ -0,0 +1,37 @@
+DECLARE @tkdbh NVARCHAR(16)
+
+------手工获取退库单最大单号
+--SELECT MAX(tkdbh)
+--FROM t_tkdzb 
+
+SELECT @tkdbh = 'TK17000002'
+
+CREATE TABLE #t
+( 
+	tkdbh NVARCHAR(16),
+	ordr INT IDENTITY(1,1),
+	spbh NVARCHAR(32),
+	pcbh NVARCHAR(32),
+	shul INT,
+	guiw NVARCHAR(8),
+	tkyy NVARCHAR(32),
+	yxrq DATETIME NULL,
+	scrq DATETIME NULL,
+	jg DECIMAL(16,2)
+)
+
+INSERT INTO #t(spbh, pcbh, shul, guiw, yxrq, scrq,jg)
+SELECT c.SPBH, c.PCBH, c.CHSL, c.HWBH, c.yxrq, c.scrq,c.jiag
+FROM t_chxx c
+
+----插入退库单主表
+INSERT INTO t_tkdzb(tkdbh,tkrq,sqr,flag,bz)
+SELECT @tkdbh,CONVERT(varchar(100), GETDATE(), 23),'001',1,N'广安门装修，系统生成退库'
+
+----插入退库单明细表
+INSERT INTO t_tkdmxb(TKDBH, ORDR, SPBH, PCBH, JHJG, SHUL, GUIW, TKYY, yxrq, scrq)
+SELECT @tkdbh,ordr,spbh, pcbh, jg, shul, guiw, '', yxrq, scrq
+FROM #t
+
+DROP TABLE #t
+
