322df378a7526142147f573d41fe64eee73c290a
ljy607 <26521794@qq.com>
2017-08-17 17:22:23 +0800

    2017年8月17日 17:22:21

--- sql/修改单据人员.sql
+++ sql/修改单据人员.sql
@@ -1,52 +1,98 @@
------三店-------------------
---来货表
-SELECT *
---UPDATE z SET ysr = '042' ---'019'
-FROM t_ckdzb z
-WHERE ckrq BETWEEN '2012-01-01' AND '2014-11-07 23:59:59'
-SELECT *
---UPDATE z SET ysr = '124' ---'042'
-FROM t_ckdzb z
-WHERE ckrq BETWEEN '2014-11-08' AND GETDATE()
-
---效期催销表
-SELECT *
---UPDATE z SET z.zgr = '042'  ---'019'
-FROM T_CXZB z
-WHERE cxrq BETWEEN '2012-01-01' AND '2014-11-07 23:59:59'
-SELECT *
---UPDATE z SET z.zgr = '124'  ---'042'
-FROM T_CXZB z
-WHERE cxrq BETWEEN '2014-11-08' AND GETDATE()
-
-----损益表
-SELECT *
---UPDATE z SET pzr = '042' --'019'
-FROM t_bsdzb z
-WHERE bsrq BETWEEN '2012-01-01' AND '2014-11-07 23:59:59'
-SELECT *
---UPDATE z SET pzr = '124' --'042'
-FROM t_bsdzb z
-WHERE bsrq BETWEEN '2014-11-08' AND GETDATE()
-
-
------广安门-------------------------------
---来货表
+--------小屯-------------
+-------来货验收
 --SELECT *
-----UPDATE z SET ysr = '042' ---'019'
+----UPDATE z SET fhr = '005' ---'042'
 --FROM t_ckdzb z
---WHERE ckrq BETWEEN '2012-01-01' AND '2014-11-07 23:59:59'
-SELECT *
---UPDATE z SET ysr = '124' ---'042'
-FROM t_ckdzb z
-WHERE ckrq BETWEEN '2012-01-01' AND GETDATE()
+--WHERE ckrq  BETWEEN '2017-01-01' AND '2017-7-31 23:59:59'
 
---效期催销表
-SELECT *
---UPDATE z SET z.zgr = '042'  ---'019'
-FROM T_CXZB z
-WHERE cxrq BETWEEN '2012-01-01' AND '2014-11-07 23:59:59'
-SELECT *
---UPDATE z SET z.zgr = '124'  ---'042'
-FROM T_CXZB z
-WHERE cxrq BETWEEN '2014-11-08' AND GETDATE()
+--SELECT *
+----UPDATE z SET fhr = '005' ---'042'
+--FROM t_ckdzb z
+--WHERE ckrq  BETWEEN '2017-08-01' AND GETDATE()
+
+
+------二店-------------
+----退库表
+--SELECT *
+----UPDATE z SET sqr = '097' ---'019'
+--FROM t_tkdzb z
+--WHERE tkrq >= '2016-06-09' 
+
+-------来货验收
+--SELECT *
+----UPDATE z SET ysr = '097' ---'042'
+--FROM t_ckdzb z
+--WHERE ckrq  >= '2016-06-09' 
+
+------效期催销表
+--SELECT *
+----UPDATE z SET z.yhr = '097'  ---'019'
+--FROM T_CXZB z
+--WHERE cxrq >= '2016-06-09' 
+
+--------养护记录
+--SELECT *
+----UPDATE ty SET ty.yhr = '097'
+--FROM t_yhjlzb ty
+--WHERE ty.YHRQ >= '2016-06-09'
+
+----------要货计划
+--SELECT *
+----UPDATE z SET z.zbr = '097'
+--FROM t_yhjhzb z
+--WHERE z.yhrq >= '2016-06-09'
+
+
+-------三店-------------------
+------来货表
+--SELECT *
+----UPDATE z SET fhr = '124' ---'019'
+--FROM t_ckdzb z
+--WHERE ckrq BETWEEN '2016-01-01' AND '2017-07-31 23:59:59'
+
+--SELECT *
+----UPDATE z SET ysr = '124' ---'042'
+--FROM t_ckdzb z
+--WHERE ckrq >= '2017-08-01'
+
+----效期催销表
+--SELECT *
+----UPDATE z SET z.zgr = '042'  ---'019'
+--FROM T_CXZB z
+--WHERE cxrq BETWEEN '2012-01-01' AND '2014-11-07 23:59:59'
+--SELECT *
+----UPDATE z SET z.zgr = '124'  ---'042'
+--FROM T_CXZB z
+--WHERE cxrq BETWEEN '2014-11-08' AND GETDATE()
+
+------损益表
+--SELECT *
+----UPDATE z SET pzr = '042' --'019'
+--FROM t_bsdzb z
+--WHERE bsrq BETWEEN '2012-01-01' AND '2014-11-07 23:59:59'
+--SELECT *
+----UPDATE z SET pzr = '124' --'042'
+--FROM t_bsdzb z
+--WHERE bsrq BETWEEN '2014-11-08' AND GETDATE()
+
+
+-------广安门-------------------------------
+----来货表
+--SELECT *
+----UPDATE z SET fhr = '030' ---'019'
+--FROM t_ckdzb z
+--WHERE ckrq BETWEEN '2016-01-01' AND '2017-05-30 23:59:59'
+--SELECT *
+----UPDATE z SET ysr = '124' ---'042'
+--FROM t_ckdzb z
+--WHERE ckrq between '2017-05-01' AND GETDATE()
+
+----效期催销表
+--SELECT *
+----UPDATE z SET z.zgr = '042'  ---'019'
+--FROM T_CXZB z
+--WHERE cxrq BETWEEN '2012-01-01' AND '2014-11-07 23:59:59'
+--SELECT *
+----UPDATE z SET z.zgr = '124'  ---'042'
+--FROM T_CXZB z
+--WHERE cxrq BETWEEN '2014-11-08' AND GETDATE()
