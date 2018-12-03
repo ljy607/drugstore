751ae9b4928345771da1ff1aac3e2ceaf485899a
ljy607 <26521794@qq.com>
2018-12-03 17:32:48 +0800

    2018年12月3日 17:32:44

--- sql/zd_proc.sql
+++ sql/zd_proc.sql
@@ -1,96 +1,97 @@
----------------2018年11月25日 15:13:55-----------------------
------修改要货计划供应商选择，提醒询价日期  2018年11月25日 15:14:52
+-----------------2018年11月25日 15:13:55-----------------------
+-------修改要货计划供应商选择，提醒询价日期  2018年11月25日 15:14:52
 
-/******************* 过程说明 *****************************************************************
-  要货计划录入时获取供应商价格 
-  参数	@spbh   	货号
-  结果	生成供应商价格记录信息
-**********************************************************************************************/
-ALTER PROCEDURE [dbo].[sp_yhjh_gysjg] @spbh NVARCHAR(16) AS
-begin
+--/******************* 过程说明 *****************************************************************
+--  要货计划录入时获取供应商价格 
+--  参数	@spbh   	货号
+--  结果	生成供应商价格记录信息
+--**********************************************************************************************/
+--ALTER PROCEDURE [dbo].[sp_yhjh_gysjg] @spbh NVARCHAR(16) AS
+--begin
 
-CREATE TABLE #t 
-(
-	gysbh NVARCHAR(16),
-	jhj DECIMAL(14,4) null DEFAULT 0,
-	zhjj NVARCHAR(32) NULL DEFAULT 0,
-	xjjg DECIMAL(14,4) null DEFAULT 0,
-	gsyzrq DATETIME NULL,
-	jyxkzrq DATETIME NULL,
-	gsprq DATETIME NULL,
-	frrq DATETIME NULL,
-	xyrq DATETIME NULL,
-	xjrq DATETIME NULL
-)
+--CREATE TABLE #t 
+--(
+--	gysbh NVARCHAR(16),
+--	jhj DECIMAL(14,4) null DEFAULT 0,
+--	zhjj NVARCHAR(32) NULL DEFAULT 0,
+--	xjjg DECIMAL(14,4) null DEFAULT 0,
+--	gsyzrq DATETIME NULL,
+--	jyxkzrq DATETIME NULL,
+--	gsprq DATETIME NULL,
+--	frrq DATETIME NULL,
+--	xyrq DATETIME NULL,
+--	xjrq DATETIME NULL
+--)
 
-CREATE TABLE #tt
-(
-	gysbh NVARCHAR(16),
-	xjjg DECIMAL(14,4) NULL DEFAULT 0,
-	xjrq DATETIME NULL
-)
-
-
-
-INSERT INTO #t(gysbh, jhj, zhjj, gsyzrq, jyxkzrq, gsprq, frrq, xyrq)
-SELECT z.GYSBH,min(m.jhj) jhj2year ,
-(select top 1 cast(e.jhj AS NVARCHAR(16)) +'  '+ convert(varchar(10),e1.jhrq,102) from t_jhdmxb e join t_jhdzb e1 on e.jhdbh = e1.jhdbh 
-where e.spbh = @spbh and e1.gysbh=z.gysbh and e1.jhrq > dateadd(month,-6,getdate()) order by e1.jhrq desc ) as zhjj,
-g.gsyzrq,g.jyxkzrq,g.gsprq,g.frrq,g.xyrq
-FROM  T_JHDZB z,t_jhdmxb m,t_gysxx g
-WHERE z.jhdbh = m.jhdbh and
-	   z.gysbh = g.gysbh and
-		m.spbh = @spbh and
-		z.jhrq > dateadd(month,-24,getdate())
-group by z.gysbh,g.gsyzrq,g.jyxkzrq,g.gsprq,g.frrq,g.xyrq
-order by min(m.jhj)
-
------询价价格
-INSERT INTO #tt(gysbh, xjjg,xjrq)
-SELECT t.gysbh,t1.jg,t.rq
-FROM t_xjdzb AS T
-JOIN t_xjdmxb AS t1 ON t1.xjdbh = T.xjdbh 
-JOIN t_gysxx AS g ON g.gysbh = T.gysbh
-WHERE t1.spbh = @spbh and 1>(SELECT COUNT(*) 
-         FROM t_xjdzb e 
-         JOIN t_xjdmxb e1 ON e1.xjdbh = e.xjdbh
-         WHERE e.gysbh=T.gysbh AND t1.spbh = e1.spbh AND e.rq>T.rq
-		)
-
-UPDATE e
-SET e.xjjg = e1.xjjg,e.xyrq = e1.xjrq
-FROM #t e
-JOIN #tt e1 ON e.gysbh = e1.gysbh
-
-INSERT INTO #t(gysbh, xjjg, gsyzrq, jyxkzrq, gsprq, frrq,xyrq,xjrq)
-SELECT e1.GYSBH,e.xjjg,e1.gsyzrq, e1.jyxkzrq, e1.gsprq, e1.frrq, e1.xyrq,e.xjrq
-FROM #tt e
-JOIN t_gysxx e1 ON e1.gysbh = e.gysbh
-LEFT JOIN #t e2 ON e2.gysbh = e.gysbh
-WHERE e2.gysbh IS NULL
-
-
-SELECT * FROM #t
-
-DROP TABLE #t
-DROP TABLE #tt
-
-END
-
-GO
+--CREATE TABLE #tt
+--(
+--	gysbh NVARCHAR(16),
+--	xjjg DECIMAL(14,4) NULL DEFAULT 0,
+--	xjrq DATETIME NULL
+--)
 
 
 
---------------2018年11月12日 16:46:44-----------------
-------获取商品判断状态，只有flag=1的商品可以查询到
-ALTER PROCEDURE [dbo].[SP_GETSPXX] @spbh varchar(16) AS
-SELECT  T_SPXX.SPBH, T_SPXX.PM,T_SPXX.GG, T_JLDW.JLDW, T_SPXX.JC, T_SCCJ.JC as SCCJ,T_JGXX.LSJ, T_JGXX.PFJ, T_JGXX.GBJ
-FROM T_SPXX LEFT OUTER JOIN
-      T_JLDW ON T_SPXX.JLDWBH = T_JLDW.JLDWBH LEFT OUTER JOIN
-      T_SCCJ ON T_SPXX.CJBH = T_SCCJ.CJBH LEFT OUTER JOIN
-      T_JGXX ON T_SPXX.SPBH = T_JGXX.SPBH
-WHERE t_spxx.FLAG = 1 AND (T_SPXX.SPBH like '%' + @spbh + '%' OR T_SPXX.PM like '%' + @spbh + '%' OR T_SPXX.JC like '%' + @spbh + '%')
-order by t_spxx.spbh
+--INSERT INTO #t(gysbh, jhj, zhjj, gsyzrq, jyxkzrq, gsprq, frrq, xyrq)
+--SELECT z.GYSBH,min(m.jhj) jhj2year ,
+--(select top 1 cast(e.jhj AS NVARCHAR(16)) +'  '+ convert(varchar(10),e1.jhrq,102) from t_jhdmxb e join t_jhdzb e1 on e.jhdbh = e1.jhdbh 
+--where e.spbh = @spbh and e1.gysbh=z.gysbh and e1.jhrq > dateadd(month,-6,getdate()) order by e1.jhrq desc ) as zhjj,
+--g.gsyzrq,g.jyxkzrq,g.gsprq,g.frrq,g.xyrq
+--FROM  T_JHDZB z,t_jhdmxb m,t_gysxx g
+--WHERE z.jhdbh = m.jhdbh and
+--	   z.gysbh = g.gysbh and
+--		m.spbh = @spbh and
+--		z.jhrq > dateadd(month,-24,getdate())
+--group by z.gysbh,g.gsyzrq,g.jyxkzrq,g.gsprq,g.frrq,g.xyrq
+--order by min(m.jhj)
+
+-------询价价格
+--INSERT INTO #tt(gysbh, xjjg,xjrq)
+--SELECT t.gysbh,t1.jg,t.rq
+--FROM t_xjdzb AS T
+--JOIN t_xjdmxb AS t1 ON t1.xjdbh = T.xjdbh 
+--JOIN t_gysxx AS g ON g.gysbh = T.gysbh
+--WHERE t1.spbh = @spbh and 1>(SELECT COUNT(*) 
+--         FROM t_xjdzb e 
+--         JOIN t_xjdmxb e1 ON e1.xjdbh = e.xjdbh
+--         WHERE e.gysbh=T.gysbh AND t1.spbh = e1.spbh AND e.rq>T.rq
+--		)
+
+--UPDATE e
+--SET e.xjjg = e1.xjjg,e.xyrq = e1.xjrq
+--FROM #t e
+--JOIN #tt e1 ON e.gysbh = e1.gysbh
+
+--INSERT INTO #t(gysbh, xjjg, gsyzrq, jyxkzrq, gsprq, frrq,xyrq,xjrq)
+--SELECT e1.GYSBH,e.xjjg,e1.gsyzrq, e1.jyxkzrq, e1.gsprq, e1.frrq, e1.xyrq,e.xjrq
+--FROM #tt e
+--JOIN t_gysxx e1 ON e1.gysbh = e.gysbh
+--LEFT JOIN #t e2 ON e2.gysbh = e.gysbh
+--WHERE e2.gysbh IS NULL
+
+
+--SELECT * FROM #t
+
+--DROP TABLE #t
+--DROP TABLE #tt
+
+--END
+
+--GO
+
+
+
+----------------2018年11月12日 16:46:44-----------------
+--------获取商品判断状态，只有flag=1的商品可以查询到
+--ALTER PROCEDURE [dbo].[SP_GETSPXX] @spbh varchar(16) AS
+--SELECT  T_SPXX.SPBH, T_SPXX.PM,T_SPXX.GG, T_JLDW.JLDW, T_SPXX.JC, T_SCCJ.JC as SCCJ,T_JGXX.LSJ, T_JGXX.PFJ, T_JGXX.GBJ
+--FROM T_SPXX LEFT OUTER JOIN
+--      T_JLDW ON T_SPXX.JLDWBH = T_JLDW.JLDWBH LEFT OUTER JOIN
+--      T_SCCJ ON T_SPXX.CJBH = T_SCCJ.CJBH LEFT OUTER JOIN
+--      T_JGXX ON T_SPXX.SPBH = T_JGXX.SPBH
+--WHERE t_spxx.FLAG = 1 AND (T_SPXX.SPBH like '%' + @spbh + '%' OR T_SPXX.PM like '%' + @spbh + '%' OR T_SPXX.JC like '%' + @spbh + '%')
+--order by t_spxx.spbh
+--GO
 
 
 --------------- 2017年12月2日 16:25:16 -----------------
